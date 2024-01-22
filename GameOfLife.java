48820e092d482749b37bb9dfd71201fc9ddcb6b7
pelegOberlender <154446502+pelegOberlender@users.noreply.github.com>
2024-01-22 12:02:52 +0200

    Add files via upload

--- GameOfLife.java
+++ GameOfLife.java
@@ -148,7 +148,7 @@ public class GameOfLife {
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
 		for(int i = 1; i < arr.length - 1; i++) {
-			for(int j = 1; j < arr[i].length - 1; j++) {
+			for(int j = 1; j < arr[0].length - 1; j++) {
 				System.out.printf("%3s", arr[i][j]);
 			}	
 			System.out.println();	
